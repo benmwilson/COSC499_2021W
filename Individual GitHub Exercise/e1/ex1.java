9d2ad40c1da98bc78d8d64c2526c7b697270c188
Ben Wilson <ben@benmwilson.dev>
2021-09-16 20:02:45 -0700

    added sortInt functionality

--- Individual GitHub Exercise/e1/ex1.java
+++ Individual GitHub Exercise/e1/ex1.java
@@ -20,5 +20,23 @@ public class ex1 {
 		String words[] = ss.split(" ");
 
 		
+		if (c == 1) {
+			int[] myIntArray = new int[words.length];
 
+			for (int i = 0; i < words.length; i++) {
+				myIntArray[i] = Integer.parseInt(words[i]);
+			}
+
+			Arrays.sort(myIntArray);
+
+			System.out.println("Your sorted list of Integers: ");
+			for (int i = 0; i < words.length; i++) {
+				System.out.println(myIntArray[i]);
+			}
+
+		} else if (c == 2) {
+			
+		}
+
+	}
 }
