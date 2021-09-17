c58265fb88b351770dc0213eeb7bc0a445c02878
Ben Wilson <ben@benmwilson.dev>
2021-09-16 19:57:41 -0700

    Revert "added simple option selector"
    This reverts commit 84750037f9dab4dde1f6c4a6dbe0c7e78c7490c7.

--- Individual GitHub Exercise/e1/ex1.java
+++ Individual GitHub Exercise/e1/ex1.java
@@ -7,18 +7,6 @@ public class ex1 {
 
 	public static void main(String[] args) {
 
-		Scanner s = new Scanner(System.in);
-
-		System.out.println("Do you want to sort Integers or Strings?");
-		System.out.print("Enter 1 for int and 2 for String: ");
-		int c = s.nextInt();
-
-		System.out.print("Enter a list seperated by spaces: ");
-		s.nextLine();
-		String ss = s.nextLine();
-
-		String words[] = ss.split(" ");
-
 		
 
 }
